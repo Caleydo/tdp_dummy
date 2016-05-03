4c6e2becd5de1b2525aa2ad7fd4a9a073df9e3e0
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-05-04 00:04:42 +0200

    expression vs copynumber

---
+++ ExpressionVsCopyNumber.ts
@@ -0,0 +1,121 @@
+/**
+ * Created by Samuel Gratzl on 27.04.2016.
+ */
+/// <reference path='../../tsd.d.ts' />
+
+/// <amd-dependency path='css!./style' />
+import ajax = require('../caleydo_core/ajax');
+import {AView, IViewContext, ISelection} from '../targid2/View';
+import {sample_tumor_type, alteration_types} from './Configs';
+
+export class ExpressionVsCopyNumber extends AView {
+
+  private x = d3.scale.linear();
+  private y = d3.scale.linear();
+  private xAxis = d3.svg.axis().orient('bottom').scale(this.x);
+  private yAxis = d3.svg.axis().orient('left').scale(this.y);
+
+  private parameter = {
+    tumor_type: sample_tumor_type[0]
+  };
+
+  constructor(context:IViewContext, private selection: ISelection, parent:Element, options?) {
+    super(context, parent, options);
+
+    this.build();
+    this.update();
+  }
+
+  buildParameterUI($parent: d3.Selection<any>, onChange: (name: string, value: any)=>Promise<any>) {
+    $parent.append('span').text('tumor type ');
+    const $selectType = $parent.append('select').attr({
+      'class': 'form-control',
+      required: 'required'
+    }).on('change', function() {
+      onChange('tumor_type', sample_tumor_type[this.selectedIndex]);
+    });
+    $selectType.selectAll('option').data(sample_tumor_type)
+      .enter().append('option').text(String).attr('value', String);
+    $selectType.property('selectedIndex', sample_tumor_type.indexOf(this.parameter.tumor_type));
+
+  }
+
+  getParameter(name: string): any {
+    return this.parameter[name];
+  }
+
+  setParameter(name: string, value: any) {
+    this.parameter[name] = value;
+    return this.update();
+  }
+
+  private build() {
+
+    const margin = {top: 10, right: 30, bottom: 30, left: 30},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+
+    var svg = this.$node.append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    this.x.range([0, width]);
+    this.y.range([height, 0]);
+
+    svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + height + ')');
+    svg.append('g')
+      .attr('class', 'y axis')
+  }
+
+
+  private updateChart(rows: {value1: number, value2: number}[]) {
+    this.x.domain(d3.extent(rows, (d) => d.value1));
+    this.y.domain(d3.extent(rows, (d) => d.value2));
+
+    const svg = this.$node.select('svg g');
+
+    svg.select('g.x.axis').call(this.xAxis);
+    svg.select('g.y.axis').call(this.yAxis);
+
+    const marks = svg.selectAll('.mark').data(rows);
+    marks.enter().append('circle').classed('mark', true).attr('r', 5);
+
+    marks.transition().attr({
+      cx : (d) => this.x(d.value1),
+      cy : (d) => this.y(d.value2),
+    });
+
+    marks.exit().remove();
+
+  }
+
+  changeSelection(selection:ISelection) {
+    this.selection = selection;
+    return this.update();
+  }
+
+  private update() {
+    const idtype = this.selection.idtype;
+    this.setBusy(true);
+    return this.resolveId(idtype, this.selection.range.first, 'IDTypeA').then((name) => {
+      return ajax.getAPIJSON('/targid/db/dummy/expression_vs_copynumber', {
+        a_id: name,
+        b_cat2 : this.parameter.tumor_type
+      });
+    }).then((rows) => {
+      this.updateChart(rows);
+      this.setBusy(false);
+    });
+  }
+}
+
+
+export function create(context:IViewContext, selection:ISelection, parent:Element, options?) {
+  return new ExpressionVsCopyNumber(context, selection, parent, options);
+}
+
+
