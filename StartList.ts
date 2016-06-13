a3b45daa4ea76fb70724ca2109dad4496f6bf24b
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-06-13 17:06:23 +0200

    fix Caleydo/targid2#47 named entity set loading

--- StartList.ts
+++ StartList.ts
@@ -6,22 +6,17 @@
 import ajax = require('../caleydo_core/ajax');
 import d3 = require('d3');
 import idtypes = require('../caleydo_core/idtype');
-import ranges = require('../caleydo_core/range');
 import {IViewContext, ISelection} from '../targid2/View';
-import {ALineUpView, stringCol, categoricalCol, numberCol2, useDefaultLayout, booleanCol} from '../targid2/LineUpView';
+import {ALineUpView, stringCol, categoricalCol, numberCol2, useDefaultLayout} from '../targid2/LineUpView';
 import {gene_species} from './Configs';
 import {listNamedSets} from '../targid2/storage';
 
 
 export class AStart extends ALineUpView {
-  private filter = ranges.Range1D.none();
-  private filterName = 'F';
   private cat : string = null;
 
   constructor(context:IViewContext, selection: ISelection, parent:Element, options?) {
     super(context, parent, options);
-    this.filter = (options && options.filter ? ranges.parse(options.filter) : ranges.none()).dim(0);
-    this.filterName = options && options.filterName ? options.filterName : 'F';
     this.cat = options && options.cat ? options.cat : null;
     this.build();
   }
@@ -40,19 +35,8 @@ export class AStart extends ALineUpView {
         numberCol2('a_int', desc.columns.a_int.min, desc.columns.a_int.max),
         numberCol2('a_real', desc.columns.a_real.min, desc.columns.a_real.max),
       ];
-      if (!this.filter.isNone && !this.filter.isAll) {
-        columns.splice(1, 0, booleanCol('_checked', this.filterName));
-        rows.forEach((row) => row._checked = this.filter.contains(row._id));
-      }
       var lineup = this.buildLineUp(rows, columns, idtypes.resolve(desc.idType),(d) => d._id);
       useDefaultLayout(lineup);
-      if (!this.filter.isNone && !this.filter.isAll) {
-        //activate the filter to be just the checked ones
-        const f = lineup.data.getRankings()[0].flatColumns.filter((col) => col.desc.type === 'boolean' && col.desc.column === '_checked')[0];
-        if (f) {
-          f.setFilter(true);
-        }
-      }
       this.initializedLineUp();
       this.setBusy(false);
     });
