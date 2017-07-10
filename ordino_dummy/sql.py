04c9ba1dd03e0b8403dc2456a02474aa88463dd3
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-07-10 16:08:23 +0200

    provide meta data about replacements and filters

--- ordino_dummy/sql.py
+++ ordino_dummy/sql.py
@@ -14,13 +14,15 @@ agg_score = DBViewBuilder().query('%(agg)s(%(score)s)').replace('agg').replace('
 _column_query_a = 'cast(id as text) as id, a_name, a_cat1, a_cat2, a_int, a_real'
 _column_query_b = 'cast(id as text) as id, b_name, b_cat1, b_cat2, b_int, b_real'
 
-def _create_base(result, prefix, idtype):
+def _create(result, prefix, idtype, other_prefix):
+  columns = [prefix + '_name', prefix + '_cat1', prefix + '_cat2', prefix + '_int', prefix + '_real']
+  other_columns = [other_prefix + '_name', other_prefix + '_cat1', other_prefix + '_cat2', other_prefix + '_int', other_prefix + '_real']
 
   result[prefix + '_items'] = DBViewBuilder().idtype(idtype).query("""
       SELECT cast(id as text) as id, %(column)s AS text
       FROM {table} WHERE LOWER(%(column)s) LIKE :query
       ORDER BY %(column)s ASC LIMIT %(limit)s OFFSET %(offset)s""".format(table=prefix))\
-    .replace("column").replace("limit").replace("offset")\
+    .replace("column", columns).replace("limit", int).replace("offset", int)\
     .arg("query").build()
 
   result[prefix+'_items_verify'] = DBViewBuilder().idtype(idtype).query("""
@@ -35,17 +37,14 @@ def _create_base(result, prefix, idtype):
           FROM {table} WHERE LOWER(%(column)s) LIKE :query
           ) as t
         ORDER BY d ASC LIMIT %(limit)s OFFSET %(offset)s""".format(table=prefix)) \
-    .replace("column").replace("limit").replace("offset") \
+    .replace("column", columns).replace("limit", int).replace("offset", int) \
     .arg("query").build()
 
   result[prefix + '_unique_all'] = DBViewBuilder().query("""
         SELECT distinct %(column)s AS text
         FROM {table} ORDER BY %(column)s ASC """.format(table=prefix)) \
-    .replace("column").build()
+    .replace("column", columns).build()
 
-
-def _create(result, prefix, idtype, other_prefix):
-  _create_base(result, prefix, idtype)
   result[prefix + '_score'] = DBViewBuilder().idtype(idtype).query("""
     SELECT cast(e.{table}_id as text) as id, %(agg_score)s AS score
     FROM ab e
@@ -53,10 +52,11 @@ def _create(result, prefix, idtype, other_prefix):
     JOIN {other_table} s ON s.id = e.{other_table}_id
     %(where)s
     GROUP BY internal_id, p.name""".format(table=prefix, other_table=other_prefix))\
-    .replace('where').replace('agg_score').replace('data_subtype')\
-    .query('filter_rid', 'p.' + prefix +'_name %(operator)s %(value)s') \
-    .query('filter_name', 's.' + other_prefix +'name %(operator)s %(value)s') \
-    .query('filter_id', 'p.id %(operator)s %(value)s') \
+    .replace('where').replace('agg_score').replace('data_subtype', ['ab_real', 'ab_int']) \
+    .filters(*other_columns) \
+    .filter('rid', 'p.' + prefix +'_name %(operator)s %(value)s') \
+    .filter('name', 's.' + other_prefix +'name %(operator)s %(value)s') \
+    .filter('id', 'p.id %(operator)s %(value)s') \
     .build()
 
   result[prefix + '_single_score'] = DBViewBuilder().idtype(idtype).query("""
@@ -65,9 +65,10 @@ def _create(result, prefix, idtype, other_prefix):
     JOIN {table} p ON e.{table}_id = p.id
     JOIN {other_table} s ON s.id = e.{other_table}_id
     WHERE e.{other_table}_id = :name %(and_where)s""".format(table=prefix, other_table=other_prefix))\
-    .arg('name').replace('attribute').replace('and_where') \
-    .query('filter_rid', 'p.' + prefix +'_name %(operator)s %(value)s') \
-    .query('filter_id', 'p.id %(operator)s %(value)s') \
+    .arg('name').replace('attribute', ['ab_real', 'ab_int', 'ab_cat']).replace('and_where') \
+    .filters(*other_columns) \
+    .filter('rid', 'p.' + prefix +'_name %(operator)s %(value)s') \
+    .filter('id', 'p.id %(operator)s %(value)s') \
     .build()
 
 views = dict(
