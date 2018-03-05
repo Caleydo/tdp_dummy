00d0c4cdb797e1e3010cee1b76f2ee50b574fe96
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-03-05 13:03:29 +0100

    add filter example

--- src/views/TestRankingView.ts
+++ src/views/TestRankingView.ts
@@ -2,6 +2,8 @@ import {getTDPDesc, getTDPRows, IRow, IServerColumn} from 'tdp_core/src/rest';
 import {ISelection, IViewContext} from 'tdp_core/src/views/index';
 import ARankingView, {IRankingWrapper, DataProvider} from 'tdp_core/src/lineup/ARankingView';
 import {linkCol, stringCol} from 'tdp_core/src/lineup';
+import CategoricalColumn from 'lineupjs/src/model/CategoricalColumn';
+import NumberColumn from 'lineupjs/src/model/NumberColumn';
 
 export default class TestRankingView extends ARankingView {
   constructor(context: IViewContext, selection: ISelection, parent: HTMLElement, options = {}) {
@@ -59,5 +61,18 @@ export default class TestRankingView extends ARankingView {
   protected customizeRanking(ranking: IRankingWrapper) {
     ranking.groupBy('B Cat1');
     ranking.sortBy('B Int', false);
+
+    const ccol = <CategoricalColumn>ranking.findColumn('B Cat1');
+    ccol.setFilter({
+      filter: ['BCat 1', 'BCat 2'], // list all categories that should be included
+      filterMissing: true
+    });
+
+    const ncol = <NumberColumn>ranking.findColumn('B Int');
+    ncol.setFilter({
+      min: 50,
+      max: 100,
+      filterMissing: true
+    });
   }
 }
