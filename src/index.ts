8b27f9d8e7ece00237754e7e3f554a47c2dcc225
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-14 17:14:31 +0200

    add main index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export * from './scores';
+export * from './views';
+
+export * from './config';
+export * from './DummyMenuSection';
+export {default as DummyMenuSection} from './DummyMenuSection';
+export * from './DummySearchProvider';
+export {default as DummySearchProvider} from './DummySearchProvider';
